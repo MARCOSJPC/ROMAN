15e510f8cb3607a826eb23468c71afdd1c5e62d4
unknown <45444611899>
2021-11-25 17:08:36 -0300

    banco feito

---
+++ Banco/Scripts/DML_02.sql
@@ -0,0 +1,16 @@
+USE D_ROMAN;
+GO
+
+INSERT INTO Usuario(Nome,Email,Senha)
+VALUES ('Marcos Paulo','Mar@gmail.com','12345'),
+	   ('Pedro','P@gmail.com','12345')
+
+INSERT INTO TIPO_USUARIO(nomeDescricao)
+VALUES ('Administardor'), 
+		('Professor')
+
+INSERT INTO Temas(nomeTema)
+VALUES('Gestão'),('Mobile'),('FrontEnd')
+
+INSERT INTO Projeto(nomeProjeto,IdTema,IdUsuario)
+VALUES('Roman',2,1),('Opflix',3,2)
---
+++ Banco/Scripts/DQL_03.sql
@@ -0,0 +1,9 @@
+USE D_ROMAN;
+GO
+
+
+select * from Usuario
+select * from TIPO_USUARIO
+select * from Projeto
+select * from Temas
+
