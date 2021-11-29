5aa69b62b9fcda2d369498091a66fadfcb045800
Marcos Paulo <54601033+MARCOSJPC@users.noreply.github.com>
2021-11-29 14:14:28 -0300

    Add files via upload

---
+++ Back-End/Roman_api/roman_mobile_api/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roman_mobile_api.ViewModels
+{
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// Classe responsavel pelo modelo de login
+        /// </summary>
+   
+        [Required(ErrorMessage = "Informe o e-mail do Usuario")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha do Usuario")]
+        public string Senha { get; set; }
+    }
+}
